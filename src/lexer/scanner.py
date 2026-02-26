b4799dd2bb0a3fb21f719f29902ced437baa2d37
ksesha-kr <zelenayatravka3@gmail.com>
2026-02-26 13:52:39 +0300

    Update scanner.py

--- src/lexer/scanner.py
+++ src/lexer/scanner.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any, Optional
 from .token import Token, TokenType
 
 
@@ -21,23 +21,32 @@ class Scanner:
 
     def __init__(self, source: str):
         self.source = source
-        self.tokens = []
-        self.errors = []
+        self.tokens: List[Token] = []
+        self.errors: List[str] = []
         self.start = 0
         self.current = 0
         self.line = 1
         self.column = 1
-        self.scan_tokens()
+        self.start_column = 1
+        self._scan_tokens()
 
-    def scan_tokens(self):
-        while not self.is_at_end():
+    def _scan_tokens(self):
+        while not self._is_at_end():
             self.start = self.current
-            self.scan_token()
+            self.start_column = self.column
+            self._scan_token()
 
-        self.tokens.append(Token(TokenType.END_OF_FILE, "", self.line + 1, 1))
+        eof_line = self.line
+        eof_col = self.column
 
-    def scan_token(self):
-        c = self.advance()
+        if self.column != 1:
+            eof_line += 1
+            eof_col = 1
+
+        self.tokens.append(Token(TokenType.END_OF_FILE, "", eof_line, eof_col))
+
+    def _scan_token(self):
+        c = self._advance()
 
         if c == ' ' or c == '\r' or c == '\t':
             pass
@@ -47,247 +56,307 @@ class Scanner:
             return
 
         elif c == '/':
-            if self.match('/'):
-                while self.peek() != '\n' and not self.is_at_end():
-                    self.advance()
-            elif self.match('*'):
-                self.multiline_comment()
+            if self._match('/'):
+                while self._peek() != '\n' and not self._is_at_end():
+                    self._advance()
+            elif self._match('*'):
+                self._multiline_comment()
             else:
-                self.add_token(TokenType.OP_DIV)
+                self._add_token(TokenType.OP_DIV)
 
         elif c == '+':
-            self.add_token(TokenType.OP_PLUS)
+            self._add_token(TokenType.OP_PLUS)
         elif c == '-':
-            if self.peek().isdigit():
-                self.scan_number()
+            if self._peek().isdigit():
+                self._scan_negative_number()
             else:
-                self.add_token(TokenType.OP_MINUS)
+                self._add_token(TokenType.OP_MINUS)
         elif c == '*':
-            self.add_token(TokenType.OP_MUL)
+            self._add_token(TokenType.OP_MUL)
         elif c == '%':
-            self.add_token(TokenType.OP_MOD)
+            self._add_token(TokenType.OP_MOD)
         elif c == '(':
-            self.add_token(TokenType.LPAREN)
+            self._add_token(TokenType.LPAREN)
         elif c == ')':
-            self.add_token(TokenType.RPAREN)
+            self._add_token(TokenType.RPAREN)
         elif c == '{':
-            self.add_token(TokenType.LBRACE)
+            self._add_token(TokenType.LBRACE)
         elif c == '}':
-            self.add_token(TokenType.RBRACE)
+            self._add_token(TokenType.RBRACE)
         elif c == '[':
-            self.add_token(TokenType.LBRACKET)
+            self._add_token(TokenType.LBRACKET)
         elif c == ']':
-            self.add_token(TokenType.RBRACKET)
+            self._add_token(TokenType.RBRACKET)
         elif c == ';':
-            self.add_token(TokenType.SEMICOLON)
+            self._add_token(TokenType.SEMICOLON)
         elif c == ',':
-            self.add_token(TokenType.COMMA)
+            self._add_token(TokenType.COMMA)
+
         elif c == '.':
-            self.add_token(TokenType.DOT)
+            self._add_token(TokenType.DOT)
 
         elif c == '=':
-            if self.match('='):
-                self.add_token(TokenType.OP_EQ)
+            if self._match('='):
+                self._add_token(TokenType.OP_EQ)
             else:
-                self.add_token(TokenType.OP_ASSIGN)
+                self._add_token(TokenType.OP_ASSIGN)
         elif c == '!':
-            if self.match('='):
-                self.add_token(TokenType.OP_NE)
+            if self._match('='):
+                self._add_token(TokenType.OP_NE)
             else:
-                self.add_token(TokenType.OP_NOT)
+                self._add_token(TokenType.OP_NOT)
         elif c == '<':
-            if self.match('='):
-                self.add_token(TokenType.OP_LE)
+            if self._match('='):
+                self._add_token(TokenType.OP_LE)
             else:
-                self.add_token(TokenType.OP_LT)
+                self._add_token(TokenType.OP_LT)
         elif c == '>':
-            if self.match('='):
-                self.add_token(TokenType.OP_GE)
+            if self._match('='):
+                self._add_token(TokenType.OP_GE)
             else:
-                self.add_token(TokenType.OP_GT)
+                self._add_token(TokenType.OP_GT)
         elif c == '&':
-            if self.match('&'):
-                self.add_token(TokenType.OP_AND)
+            if self._match('&'):
+                self._add_token(TokenType.OP_AND)
             else:
-                self.error(f"Недопустимый символ '&'")
+                self._error(c, "Недопустимый символ '&'")
         elif c == '|':
-            if self.match('|'):
-                self.add_token(TokenType.OP_OR)
+            if self._match('|'):
+                self._add_token(TokenType.OP_OR)
             else:
-                self.error(f"Недопустимый символ '|'")
+                self._error(c, "Недопустимый символ '|'")
 
         elif c == '"':
-            self.scan_string()
+            self._scan_string()
 
         elif c.isdigit():
-            self.scan_number()
+            self._scan_number()
 
         elif c.isalpha() or c == '_':
-            self.scan_identifier()
+            self._scan_identifier()
 
         else:
-            self.error(f"Недопустимый символ '{c}'")
+            self._error(c, f"Недопустимый символ '{c}'")
 
-    def scan_identifier(self):
-        while self.peek().isalnum() or self.peek() == '_':
-            self.advance()
+    def _scan_negative_number(self):
+        while self._peek().isdigit():
+            self._advance()
+
+        is_float = False
+        if self._peek() == '.':
+            next_char = self._peek_next()
+            if next_char.isdigit():
+                is_float = True
+                self._advance()
+                while self._peek().isdigit():
+                    self._advance()
+
+                if self._peek() == '.':
+                    while True:
+                        p = self._peek()
+                        if p.isspace() or p in ';\n\r\t(){}[],+-*/%=&|!<>':
+                            break
+                        if p == '\0': break
+                        self._advance()
+                    text = self.source[self.start:self.current]
+                    self._error(text, f"Некорректное число: '{text}'")
+                    return
+            elif next_char == '.':
+                pass
+            else:
+                is_float = True
+                self._advance()
+
+        text = self.source[self.start:self.current]
+
+        if is_float:
+            try:
+                val = float(text)
+                self.tokens.append(Token(TokenType.FLOAT_LITERAL, text, self.line, self.start_column, val))
+            except ValueError:
+                self._error(text, f"Некорректное число: '{text}'")
+        else:
+            self._add_int_token(text, self.line, self.start_column)
+
+    def _scan_identifier(self):
+        while self._peek().isalnum() or self._peek() == '_':
+            self._advance()
 
         text = self.source[self.start:self.current]
 
         if len(text) > 255:
-            self.error("Слишком длинный идентификатор")
+            self.errors.append(f"Ошибка на {self.line}:{self.start_column}: Слишком длинный идентификатор")
+            self.tokens.append(
+                Token(TokenType.IDENTIFIER, text, self.line, self.start_column, "Слишком длинный идентификатор"))
             return
 
         token_type = self.keywords.get(text, TokenType.IDENTIFIER)
-        self.add_token(token_type)
+        self._add_token(token_type)
 
-    def scan_number(self):
+    def _scan_number(self):
         start_line = self.line
-        start_col = self.column - 1
+        start_col = self.start_column
 
-        number_start = self.current - 1
+        while self._peek().isdigit():
+            self._advance()
 
-        while self.peek().isdigit():
-            self.advance()
+        if self._peek() == '.':
+            next_char = self._peek_next()
 
-        dot_count = 0
+            if next_char.isdigit():
+                self._advance()
+                while self._peek().isdigit():
+                    self._advance()
 
-        while True:
-            if self.peek() == '.':
-                dot_count += 1
-                self.advance()
+                if self._peek() == '.':
+                    while True:
+                        p = self._peek()
+                        if p.isspace() or p in ';\n\r\t(){}[],+-*/%=&|!<>':
+                            break
+                        if p == '\0': break
+                        self._advance()
 
-                if not self.peek().isdigit():
-                    text = self.source[number_start:self.current]
-                    self.errors.append(f"Ошибка на {start_line}:{start_col}: Некорректное число")
-                    self.tokens.append(Token(TokenType.ERROR, text, start_line, start_col,
-                                             f"Некорректное число: '{text}'"))
+                    text = self.source[self.start:self.current]
+                    self._error(text, f"Некорректное число: '{text}'")
                     return
 
-                while self.peek().isdigit():
-                    self.advance()
-            else:
-                break
-
-        text = self.source[number_start:self.current]
-
-        if dot_count > 1:
-            self.errors.append(f"Ошибка на {start_line}:{start_col}: Некорректное число")
-            self.tokens.append(Token(TokenType.ERROR, text, start_line, start_col,
-                                     f"Некорректное число: '{text}'"))
-            return
-
-        if self.peek().isalpha():
-            try:
-                if '.' in text:
+                text = self.source[self.start:self.current]
+                try:
                     value = float(text)
                     self.tokens.append(Token(TokenType.FLOAT_LITERAL, text, start_line, start_col, value))
-                else:
-                    value = int(text)
-                    if value < -2 ** 31 or value > 2 ** 31 - 1:
-                        self.errors.append(f"Ошибка на {start_line}:{start_col}: Число вне диапазона")
-                        self.tokens.append(Token(TokenType.ERROR, text, start_line, start_col,
-                                                 f"Целочисленный литерал {value} вне допустимого диапазона [-2³¹, 2³¹-1]"))
-                    else:
-                        self.tokens.append(Token(TokenType.INT_LITERAL, text, start_line, start_col, value))
-            except ValueError:
-                self.errors.append(f"Ошибка на {start_line}:{start_col}: Некорректное число")
-                self.tokens.append(Token(TokenType.ERROR, text, start_line, start_col,
-                                         f"Некорректное число: '{text}'"))
-            return
+                except ValueError:
+                    self._error(text, f"Некорректное число: '{text}'")
+                return
 
-        try:
-            if '.' in text:
-                value = float(text)
-                self.tokens.append(Token(TokenType.FLOAT_LITERAL, text, start_line, start_col, value))
+            elif next_char == '.':
+                text = self.source[self.start:self.current]
+                self._add_int_token(text, start_line, start_col)
+
+                dot_col = self.column
+                self.tokens.append(Token(TokenType.DOT, ".", self.line, dot_col))
+
+                self._advance()
+
+                if not self._is_at_end():
+                    self.current += 1
+
+                return
+
             else:
-                value = int(text)
-                if value < -2 ** 31 or value > 2 ** 31 - 1:
-                    self.errors.append(f"Ошибка на {start_line}:{start_col}: Число вне диапазона")
-                    self.tokens.append(Token(TokenType.ERROR, text, start_line, start_col,
-                                             f"Целочисленный литерал {value} вне допустимого диапазона [-2³¹, 2³¹-1]"))
-                else:
-                    self.tokens.append(Token(TokenType.INT_LITERAL, text, start_line, start_col, value))
+                self._advance()
+                text = self.source[self.start:self.current]
+                try:
+                    value = float(text)
+                    self.tokens.append(Token(TokenType.FLOAT_LITERAL, text, start_line, start_col, value))
+                except ValueError:
+                    self._error(text, f"Некорректное число: '{text}'")
+                return
+
+        text = self.source[self.start:self.current]
+        self._add_int_token(text, start_line, start_col)
+
+    def _add_int_token(self, text: str, line: int, col: int):
+        try:
+            val = int(text)
+            min_val = -2 ** 31
+            max_val = 2 ** 31 - 1
+
+            if val < min_val or val > max_val:
+                self._error(text, f"Целочисленный литерал {val} вне допустимого диапазона [-2³¹, 2³¹-1]")
+            else:
+                self.tokens.append(Token(TokenType.INT_LITERAL, text, line, col, val))
         except ValueError:
-            self.errors.append(f"Ошибка на {start_line}:{start_col}: Некорректное число")
-            self.tokens.append(Token(TokenType.ERROR, text, start_line, start_col,
-                                     f"Некорректное число: '{text}'"))
+            self._error(text, f"Некорректное число: '{text}'")
 
-    def scan_string(self):
+    def _scan_string(self):
         start_line = self.line
-        start_col = self.column - 1
+        start_col = self.start_column
 
-        open_quote_pos = self.start
+        while not self._is_at_end():
+            c = self._peek()
+            if c == '\n':
+                break
+            if c == '"':
+                break
+            self._advance()
 
-        while not self.is_at_end() and self.peek() != '"' and self.peek() != '\n':
-            self.advance()
-
-        if self.is_at_end() or self.peek() == '\n':
-            text = self.source[open_quote_pos:self.current]
-            display_text = text + '"' if not text.endswith('"') else text
-            self.errors.append(f"Ошибка на {start_line}:{start_col}: Незавершённая строка")
-            self.tokens.append(Token(TokenType.ERROR, display_text, start_line, start_col,
-                                     "Незавершённая строка"))
+        if self._is_at_end() or self._peek() == '\n':
+            text = self.source[self.start:self.current]
+            text = text.replace('"', '\\"')
+            self._error(text, "Незавершённая строка")
             return
 
-        self.advance()
-        text = self.source[open_quote_pos:self.current]
+        self._advance()
+        text = self.source[self.start:self.current]
         value = text[1:-1]
         self.tokens.append(Token(TokenType.STRING_LITERAL, text, start_line, start_col, value))
 
-    def multiline_comment(self):
+    def _multiline_comment(self):
         nesting = 1
+        comment_start_line = self.line
+        comment_start_col = self.start_column
 
-        while nesting > 0 and not self.is_at_end():
-            if self.peek() == '\n':
+        while nesting > 0 and not self._is_at_end():
+            if self._peek() == '\n':
                 self.line += 1
                 self.column = 1
-                self.advance()
-            elif self.peek() == '*' and self.peek_next() == '/':
-                self.advance()
-                self.advance()
+                self._advance()
+            elif self._peek() == '*' and self._peek_next() == '/':
+                self._advance()
+                self._advance()
                 nesting -= 1
-            elif self.peek() == '/' and self.peek_next() == '*':
-                self.advance()
-                self.advance()
+            elif self._peek() == '/' and self._peek_next() == '*':
+                self._advance()
+                self._advance()
                 nesting += 1
             else:
-                self.advance()
+                self._advance()
 
         if nesting > 0:
-            self.error("Незавершённый многострочный комментарий")
+            text = self.source[self.start:self.current]
+            self.errors.append(
+                f"Ошибка на {comment_start_line}:{comment_start_col}: Незавершённый многострочный комментарий")
+            self.tokens.append(Token(TokenType.ERROR, "", comment_start_line, comment_start_col,
+                                     "Незавершённый многострочный комментарий"))
 
-    def advance(self):
+    def _advance(self):
+        if self._is_at_end():
+            return '\0'
         self.current += 1
         self.column += 1
         return self.source[self.current - 1]
 
-    def match(self, expected):
-        if self.is_at_end() or self.source[self.current] != expected:
+    def _match(self, expected):
+        if self._is_at_end() or self.source[self.current] != expected:
             return False
         self.current += 1
         self.column += 1
         return True
 
-    def peek(self):
-        return '\0' if self.is_at_end() else self.source[self.current]
+    def _peek(self):
+        if self._is_at_end():
+            return '\0'
+        return self.source[self.current]
 
-    def peek_next(self):
+    def _peek_next(self):
         if self.current + 1 >= len(self.source):
             return '\0'
         return self.source[self.current + 1]
 
-    def is_at_end(self):
+    def _is_at_end(self):
         return self.current >= len(self.source)
 
-    def add_token(self, token_type):
+    def _add_token(self, token_type: TokenType, value: Any = None):
         text = self.source[self.start:self.current]
-        self.tokens.append(Token(token_type, text, self.line, self.column - len(text)))
+        col = self.start_column
+        self.tokens.append(Token(token_type, text, self.line, col, value))
 
-    def error(self, message):
-        self.errors.append(f"Ошибка на {self.line}:{self.column}: {message}")
-        text = self.source[self.start:self.current] if self.current > self.start else ""
-        self.tokens.append(Token(TokenType.ERROR, text, self.line, self.column - len(text), message))
+    def _error(self, text_or_char: str, message: str):
+        lexeme = text_or_char
+        col = self.start_column
+
+        self.errors.append(f"Ошибка на {self.line}:{col}: {message}")
+        self.tokens.append(Token(TokenType.ERROR, lexeme, self.line, col, message))
 
     def has_errors(self):
         return len(self.errors) > 0
@@ -307,8 +376,12 @@ def main():
     parser.add_argument('--output', '-o', help='Выходной файл')
     args = parser.parse_args()
 
-    with open(args.file, 'r', encoding='utf-8') as f:
-        source = f.read()
+    try:
+        with open(args.file, 'r', encoding='utf-8') as f:
+            source = f.read()
+    except FileNotFoundError:
+        print(f"Файл {args.file} не найден", file=sys.stderr)
+        sys.exit(1)
 
     scanner = Scanner(source)
 
@@ -323,7 +396,6 @@ def main():
         out.close()
 
     if scanner.has_errors():
-        print("\nОшибки:", file=sys.stderr)
         for error in scanner.errors:
             print(error, file=sys.stderr)
         sys.exit(1)
